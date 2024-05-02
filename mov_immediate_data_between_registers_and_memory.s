e77dfe50721f5c3882e479a184c2c8070c72cd6e
Muhammad Faqih Husain <88548292+fqhhusain@users.noreply.github.com>
2024-05-03 05:30:16 +0700

    Create mov_immediate_data_between_registers_and_memory.s

---
+++ mov_immediate_data_between_registers_and_memory.s
@@ -0,0 +1,19 @@
+.section .data
+    constant:
+        .int 10
+
+.section .text
+    .globl _start
+
+_start:
+    nop
+
+mov_immediate_data_between_registers_and_memory:
+    movl $777, %eax
+    movl %eax, constant
+
+exit:
+    movl $1, %eax
+    movl $0, %ebx
+    int $0x80
+
